8845a40dc86860965202da9ae42caa85cbed250e
cinchenx <cindy.yc.chen@gmail.com>
2024-03-15 11:13:08 +0800

    navbar.jsx changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,51 @@
 import { Link, Outlet } from "react-router-dom";
+import "./NavBar.css";
 
-function NavBar() {
-  return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Log In</Link>
-      </nav>
-      <Outlet />
-    </div>
-  );
+function NavBar () {
+    return (
+        <>
+           <header>
+            <nav className="navbar">
+                
+            <ul className="menu">
+                
+                <Link to="/"> Home  </Link>
+                <Link to="/login"> Log In  </Link>
+                <Link to="/createaccount"> Create Account  </Link>
+                <Link to="/projects"> Create Project  </Link>
+            </ul>
+            </nav>
+            <Outlet />
+            </header>
+          
+        </>
+
+        
+    );
 }
 
 export default NavBar;
+
+//import { Link, Outlet } from "react-router-dom";
+
+//function NavBar() {
+  //return (
+    //<div>
+      //<nav>
+        //<Link to="/">Home</Link>
+        //<Link to="/login">Log In</Link>
+      //</nav>
+      //<Outlet />
+    //</div>
+  //);
+//}
+//export default NavBar; 
+
+
+
+
+
+
+
+
+
